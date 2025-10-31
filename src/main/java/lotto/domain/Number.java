67a28334c4cc89d653be2857b86f22f21c63515e
geniusjun <geniusjun4663@gmail.com>
2025-10-31 20:42:08 +0900

    Feat(Number): 로또 숫자의 범위를 검증하는 메서드 생성
    정적 중첩 클래스를 활용하여 캡슐화와 확장성을 챙기는 검증 로직을 구현한다.

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -1,5 +1,10 @@
 package lotto.domain;
 
+import static lotto.global.constans.NumberType.MAX_LOTTO_NUMBER;
+import static lotto.global.constans.NumberType.MIN_LOTTO_NUMBER;
+
+import lotto.global.constans.ErrorMessage;
+
 public class Number {
     private int value;
 
@@ -8,6 +13,23 @@ public class Number {
     }
 
     public static Number of(int value) {
+        Validator.validate(value);
         return new Number(value);
+    }
+
+    private static class Validator {
+        private static void validate(int value) {
+            validateInvalidRange(value);
+        }
+
+        private static void validateInvalidRange(int value) {
+            if (isInvalidRange(value)) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_RANGE_ERROR.getMessage());
+            }
+        }
+
+        private static boolean isInvalidRange(int value) {
+            return value < MIN_LOTTO_NUMBER.getValue() || value > MAX_LOTTO_NUMBER.getValue();
+        }
     }
 }
