5f369ed04ae869377d93d784977259fd3fb7a50a
geniusjun <geniusjun4663@gmail.com>
2025-10-31 21:27:08 +0900

    Feat(NumberType): 로또 사이즈 상수화

--- src/main/java/lotto/global/constans/NumberType.java
+++ src/main/java/lotto/global/constans/NumberType.java
@@ -4,7 +4,9 @@ public enum NumberType {
     COST_UNIT(1000),
     MIN_LOTTO_NUMBER(1),
     MAX_LOTTO_NUMBER(45),
+    LOTTO_SIZE(6),
     ;
+    
     private final int value;
 
     NumberType(int value) {
