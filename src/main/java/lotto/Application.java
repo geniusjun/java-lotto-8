1bbc5a2b4b51c3438c6ecb0271f74c3aea88831e
geniusjun <geniusjun4663@gmail.com>
2025-10-31 17:12:41 +0900

    Feat(LottoController): 사용자의 입력을 가격으로 저장한다.
    입력이 잘 되는지 임시로 확인한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,15 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
 
-        LottoController lottoController = new LottoController(outputView);
+        LottoController lottoController = new LottoController(outputView, inputView);
+        lottoController.play();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,16 +2,20 @@ package lotto.controller;
 
 import static lotto.global.MessageType.COST_REQUEST_MESSAGE;
 
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
     private final OutputView outputView;
+    private final InputView inputView;
 
-    public LottoController(OutputView outputView) {
+    public LottoController(OutputView outputView, InputView inputView) {
         this.outputView = outputView;
+        this.inputView = inputView;
     }
 
     public void play() {
         outputView.printlnMessage(COST_REQUEST_MESSAGE.getMessage());
+        int price = Integer.parseInt(inputView.enterMessage());
     }
 }
