55f103fe4472eec1b3da221c74c821f1399a71e2
geniusjun <geniusjun4663@gmail.com>
2025-10-31 22:28:04 +0900

    Feat(MessageType): 로또 구매 갯수 메시지 상수화

--- src/main/java/lotto/global/constans/MessageType.java
+++ src/main/java/lotto/global/constans/MessageType.java
@@ -2,6 +2,7 @@ package lotto.global.constans;
 
 public enum MessageType {
     COST_REQUEST_MESSAGE("구입금액을 입력해 주세요."),
+    LOTTO_COUNT_MESSAGE("%d개를 구매했습니다."),
     ;
 
     private String message;
