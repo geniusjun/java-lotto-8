69a21fce1907d93504fae9f19319522c3c498fe1
geniusjun <geniusjun4663@gmail.com>
2025-10-31 16:47:33 +0900

    Feat(MessageType): 구입 금액 입력 요청 메시지 상수화
    매직 스트링은 앞으로 enum(MessageType)으로 관리한다.

---
+++ src/main/java/lotto/global/MessageType.java
@@ -0,0 +1,16 @@
+package lotto.global;
+
+public enum MessageType {
+    COST_REQUEST_MESSAGE("구입금액을 입력해 주세요."),
+    ;
+
+    private String message;
+
+    MessageType(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
