d41f48d8be7c391e873d9d1493faf8739102fae6
geniusjun <geniusjun4663@gmail.com>
2025-10-31 17:23:16 +0900

    Feat(InputView): 빈 문자열인지 검증한다.
    정적 메서드를 구현하여 입력 가장 초반인 InputView 단계에서 사용자의 터무니없는 빈값을 검증하도록 설계한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,19 @@
 package lotto.view;
 
+import static lotto.global.ErrorMessage.BLANK_INPUT_ERROR;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
     public String enterMessage() {
-        return Console.readLine();
+        return validateBlank(Console.readLine());
+    }
+
+    private static String validateBlank(String message) {
+        if (message.isBlank()) {
+            throw new IllegalArgumentException(BLANK_INPUT_ERROR.getMessage());
+        }
+        return message;
     }
 }
