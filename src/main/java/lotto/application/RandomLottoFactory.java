ae4b2a1e1de6ce81c62b64fa57a6fab9f1e1218b
geniusjun <geniusjun4663@gmail.com>
2025-10-31 21:44:31 +0900

    Feat(RandomLottoFactory): LottoFactory의 구현체 클래스 생성
    외부 라이브러리는 application 계층에 격리시키며 언제든지 갈아끼울 수 있는 프로그램은 구현한다.

---
+++ src/main/java/lotto/application/RandomLottoFactory.java
@@ -0,0 +1,29 @@
+package lotto.application;
+
+import static lotto.global.constans.NumberType.LOTTO_SIZE;
+import static lotto.global.constans.NumberType.MAX_LOTTO_NUMBER;
+import static lotto.global.constans.NumberType.MIN_LOTTO_NUMBER;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Number;
+
+public class RandomLottoFactory implements LottoFactory {
+    @Override
+    public Lotto creat() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                MIN_LOTTO_NUMBER.getValue(),
+                MAX_LOTTO_NUMBER.getValue(),
+                LOTTO_SIZE.getValue()
+        );
+        return Lotto.from(IntegerToNumber(numbers));
+    }
+
+    private static List<Number> IntegerToNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .map(Number::valueOf)
+                .toList();
+    }
+}
