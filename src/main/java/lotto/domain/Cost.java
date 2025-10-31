0202feef8094e149b0168cce4b96c10b47e28080
geniusjun <geniusjun4663@gmail.com>
2025-10-31 18:03:20 +0900

    Feat(Cost): 포장한 원시값 Cost 객체를 생성할 정적 팩토리 메서드 생성
    검증과 파싱을 하여 완전한 객체를 생성해서 반환하기 위해 정적 팩토리 메서드를 생성한다.

--- src/main/java/lotto/domain/Cost.java
+++ src/main/java/lotto/domain/Cost.java
@@ -1,5 +1,15 @@
 package lotto.domain;
 
+import lotto.global.Parser;
+
 public class Cost {
-    private int price;
+    private final int price;
+
+    private Cost(int price) {
+        this.price = price;
+    }
+
+    public static Cost from(String message) {
+        return new Cost(Parser.StringToInt(message));
+    }
 }
