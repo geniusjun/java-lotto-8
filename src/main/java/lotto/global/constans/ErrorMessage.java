cb5cf00412c058b3d616ac59802322ea4be6a9e2
geniusjun <geniusjun4663@gmail.com>
2025-10-31 21:58:29 +0900

    Feat(ErrorMessage): 로또 번호 개수 에러 메시지 상수화 및 숫자 포멧팅
    String.format을 활용하여 포멧팅한다.

--- src/main/java/lotto/global/constans/ErrorMessage.java
+++ src/main/java/lotto/global/constans/ErrorMessage.java
@@ -1,10 +1,14 @@
 package lotto.global.constans;
 
+import static lotto.global.constans.NumberType.COST_UNIT;
+import static lotto.global.constans.NumberType.LOTTO_SIZE;
+
 public enum ErrorMessage {
     BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다."),
     NUMBER_FORMAT_ERROR("숫자 형식이 아닙니다."),
-    INVALID_UNIT_ERROR("1000원 단위의 숫자만 입력해주세요."),
-    INVALID_RANGE_ERROR("올바르지 않은 범위의 숫자입니다.");
+    INVALID_UNIT_ERROR(String.format("%d원 단위의 숫자만 입력해주세요.", COST_UNIT.getValue())),
+    INVALID_RANGE_ERROR("올바르지 않은 범위의 숫자입니다."),
+    INVALID_LOTTO_SIZE_ERROR(String.format("%d개의 로또 번호를 입력해주세요", LOTTO_SIZE.getValue()));
 
     private static final String PREFIX = "[ERROR] ";
     private String message;
