60ff7bd417fbe1710c09a466a1303b24ef2957fe
geniusjun <geniusjun4663@gmail.com>
2025-10-31 18:33:22 +0900

    Feat(Cost): 로또 구매 가격이 1000원 단위로 나누어지는 지 검증
    정적 중첩 클래스를 구현하여 한 메서드에 하나의 역할을 다하게끔 한다.

--- src/main/java/lotto/domain/Cost.java
+++ src/main/java/lotto/domain/Cost.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static lotto.global.constans.ErrorMessage.INVALID_UNIT_ERROR;
+
 import lotto.global.Parser;
 
 public class Cost {
@@ -10,6 +12,24 @@ public class Cost {
     }
 
     public static Cost from(String message) {
-        return new Cost(Parser.StringToInt(message));
+        return new Cost(Validator.validate(message));
+    }
+
+    private static class Validator {
+        public static int validate(String cost) {
+            int value = Parser.StringToInt(cost);
+            validateUnit(value);
+            return value;
+        }
+
+        private static void validateUnit(int value) {
+            if (isNotDivisible(value)) {
+                throw new IllegalArgumentException(INVALID_UNIT_ERROR.getMessage());
+            }
+        }
+
+        private static boolean isNotDivisible(int value) {
+            return value % 1000 != 0;
+        }
     }
 }
