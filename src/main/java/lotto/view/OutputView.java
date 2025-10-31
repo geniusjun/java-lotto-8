82883ce1ae5128b1ed4907c2bbdaa4c57fb76bb1
geniusjun <geniusjun4663@gmail.com>
2025-10-31 17:43:30 +0900

    Feat(OutputView): 줄바꿈 없이 출력하는 기본 출력 메서드 생성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public void printlnMessage(String message) {
         System.out.println(message);
     }
+
+    public void printMessage(String message) {
+        System.out.print(message);
+    }
 }
