c6f15e208a566c5b4bb9883d0a0d98b715feb26c
geniusjun <geniusjun4663@gmail.com>
2025-10-31 18:01:11 +0900

    Feat(ErrorMessage): 숫자 형식이 아닌 에러메시지 상수화

--- src/main/java/lotto/global/Parser.java
+++ src/main/java/lotto/global/Parser.java
@@ -1,11 +1,13 @@
 package lotto.global;
 
+import static lotto.global.constans.ErrorMessage.NUMBER_FORMAT_ERROR;
+
 public class Parser {
     public static int StringToInt(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR.getMessage());
         }
     }
 }
--- src/main/java/lotto/global/constans/ErrorMessage.java
+++ src/main/java/lotto/global/constans/ErrorMessage.java
@@ -1,7 +1,8 @@
 package lotto.global.constans;
 
 public enum ErrorMessage {
-    BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다.");
+    BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다."),
+    NUMBER_FORMAT_ERROR("숫자 형식이 아닙니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private String message;
