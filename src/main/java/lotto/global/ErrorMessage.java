0d4cb476279dd5ac0bd0b16fbe67407d67ef2aee
geniusjun <geniusjun4663@gmail.com>
2025-10-31 17:20:12 +0900

    Feat(ErrorMessage): 에러메시지를 저장하는 Enum 클래스 생성
    PREFIX를 상수로 지정하여 생성자로 메시지를 만들시 PREFIX를 붙혀서 생성하게끔 한다.

---
+++ src/main/java/lotto/global/ErrorMessage.java
@@ -0,0 +1,16 @@
+package lotto.global;
+
+public enum ErrorMessage {
+    BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다.");
+
+    private static final String PREFIX = "[ERROR] ";
+    private String message;
+
+    ErrorMessage(String message) {
+        this.message = PREFIX + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
