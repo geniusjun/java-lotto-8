2410167e7f15b03817e507e11bbbdfd1a9bf352d
geniusjun <geniusjun4663@gmail.com>
2025-10-31 19:57:31 +0900

    Test(CostTest): 비용 생성 실패 테스트

--- src/test/java/lotto/domain/CostTest.java
+++ src/test/java/lotto/domain/CostTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.global.constans.ErrorMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,9 +12,21 @@ public class CostTest {
     @ParameterizedTest
     @ValueSource(strings = {"1000", "3000", "1000000"})
     void 비용_생성_테스트(String input) {
-        // given// when
+        // given // when
         Cost cost = Cost.of(input);
         // then
         Assertions.assertThat(cost).isNotNull();
     }
+
+    @DisplayName("1000원 단위가 아닌 비용을 입력하면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1100", "10500", "1000001"})
+    void 비용_생성_실패_테스트(String input) {
+        // given // when // then
+        Assertions.assertThatThrownBy(() -> {
+                    Cost.of(input);
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_UNIT_ERROR.getMessage());
+    }
+    
 }
