dd4015ea2742f1c0b0f27ddcb0e76759cae4c7f7
geniusjun <geniusjun4663@gmail.com>
2025-11-01 17:22:03 +0900

    Test(NumberTest): 정상적으로 Number 객체를 생성하는지 테스트

--- src/test/java/lotto/domain/NumberTest.java
+++ src/test/java/lotto/domain/NumberTest.java
@@ -3,11 +3,24 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.global.constans.ErrorMessage;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class NumberTest {
 
+    @DisplayName("정상적으로 Number 객체를 생성한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 45, 33, 20})
+    void 숫자_생성_테스트(int input) {
+        // given // when
+        Number number = Number.valueOf(input);
+        // then
+        Assertions.assertThat(number).isNotNull();
+    }
+
+    @DisplayName("범위가 넘는 숫자를 입력하면 에러가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {0, -1, 48, 100})
     void 숫자_범위_에러_테스트(int input) {
