13d001f94fc75dd664915030c739adc47514933a
geniusjun <geniusjun4663@gmail.com>
2025-11-02 15:34:22 +0900

    Feat(WinningNumber): 보너스 번호가 당첨번호와 중복되지 않은 번호인지 검증하는 기능 구현
    정적 내부 클래스를 구현하여 객체의 책임에 걸맞는 검증을 실시하게끔 설계한다.

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static lotto.global.constans.ErrorMessage.BONUS_NUMBER_DUPLICATE_ERROR;
+
 public class WinningNumbers {
     private final Lotto winning;
     private final Number bonus;
@@ -10,6 +12,21 @@ public class WinningNumbers {
     }
 
     public static WinningNumbers of(Lotto winning, Number bonus) {
+        Validator.validateDuplicate(winning, bonus);
         return new WinningNumbers(winning, bonus);
+    }
+
+    private static class Validator {
+        private static void validateDuplicate(Lotto winning, Number bonus) {
+            if (isDuplicated(winning, bonus)) {
+                throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATE_ERROR.getMessage());
+            }
+        }
+
+        private static boolean isDuplicated(Lotto winning, Number bonus) {
+            return winning.getStream()
+                    .anyMatch(number ->
+                            number.equals(bonus));
+        }
     }
 }
--- src/main/java/lotto/global/constans/ErrorMessage.java
+++ src/main/java/lotto/global/constans/ErrorMessage.java
@@ -10,7 +10,7 @@ public enum ErrorMessage {
     INVALID_UNIT_ERROR(String.format("%d원 단위의 숫자만 입력해주세요.", COST_UNIT.getValue())),
     INVALID_RANGE_ERROR("올바르지 않은 범위의 숫자입니다."),
     INVALID_LOTTO_SIZE_ERROR(String.format("%d개의 로또 번호를 입력해주세요", LOTTO_SIZE.getValue())),
-    ;
+    BONUS_NUMBER_DUPLICATE_ERROR("보너스 번호는 입력하신 당첨 번호와 중복되지 않아야 합니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private String message;
