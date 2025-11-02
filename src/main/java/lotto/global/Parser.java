76fb60407de0e99f75066b8d1c27b4019630c2a4
geniusjun <geniusjun4663@gmail.com>
2025-11-02 14:59:39 +0900

    Feat(Parser): 문자열을 Number 리스트롤 변환할 파싱 메서드 구현

--- src/main/java/lotto/global/Parser.java
+++ src/main/java/lotto/global/Parser.java
@@ -2,6 +2,10 @@ package lotto.global;
 
 import static lotto.global.constans.ErrorMessage.NUMBER_FORMAT_ERROR;
 
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.Number;
+
 public class Parser {
     public static int StringToInt(String input) {
         try {
@@ -9,5 +13,13 @@ public class Parser {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NUMBER_FORMAT_ERROR.getMessage());
         }
+    }
+
+    public static List<Number> stringToNumbers(String s) {
+        return Arrays.stream(s.split(","))
+                .map(String::trim)
+                .map(Parser::StringToInt)
+                .map(Number::valueOf)
+                .toList();
     }
 }
