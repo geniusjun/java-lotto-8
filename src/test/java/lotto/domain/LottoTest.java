37ea0b765621d2677e9b968634b096abbfb933a0
geniusjun <geniusjun4663@gmail.com>
2025-11-02 16:00:32 +0900

    Refactor(LottoTest): 로또 생성 실패 테스트 로직 수정

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
 import java.util.List;
 import lotto.global.constans.ErrorMessage;
 import org.assertj.core.api.Assertions;
@@ -14,45 +15,34 @@ class LottoTest {
     @Test
     void 로또_생성_성공() {
         // given // when
-        List<Number> numbers = List.of(
-                Number.valueOf(1),
-                Number.valueOf(8),
-                Number.valueOf(11),
-                Number.valueOf(31),
-                Number.valueOf(41),
-                Number.valueOf(42));
+        List<Number> numbers = numbersOf(1, 8, 11, 31, 41, 42);
+
         // then
-        Assertions.assertThatCode(() ->
-                Lotto.from(numbers)).doesNotThrowAnyException();
+        Assertions.assertThatCode(() -> Lotto.from(numbers))
+                .doesNotThrowAnyException();
     }
 
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다")
     @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> Lotto.from(List.of(
-                Number.valueOf(1),
-                Number.valueOf(2),
-                Number.valueOf(3),
-                Number.valueOf(4),
-                Number.valueOf(5),
-                Number.valueOf(6),
-                Number.valueOf(7))))
+    void 로또_번호_개수_초과_예외() {
+        assertThatThrownBy(() -> Lotto.from(numbersOf(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.INVALID_LOTTO_SIZE_ERROR.getMessage());
-
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다")
     @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> Lotto.from(List.of(
-                Number.valueOf(1),
-                Number.valueOf(2),
-                Number.valueOf(3),
-                Number.valueOf(4),
-                Number.valueOf(5),
-                Number.valueOf(5))))
+    void 로또_번호_중복_예외() {
+        assertThatThrownBy(() -> Lotto.from(numbersOf(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.NUMBER_DUPLICATE_ERROR.getMessage());
     }
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+    // 테스트 헬퍼 메서드
+    private List<Number> numbersOf(Integer... values) {
+        return Arrays.stream(values)
+                .map(Number::valueOf)
+                .toList();
+    }
+
 }
