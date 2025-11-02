959ff2eea90dddc7fd109c2872de62debc69e3ef
geniusjun <geniusjun4663@gmail.com>
2025-11-02 17:49:42 +0900

    Feat(Bonus): 보너스 숫자를 보관할 클래스 생성

---
+++ src/main/java/lotto/domain/Bonus.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import static lotto.global.constans.ErrorMessage.BONUS_NUMBER_DUPLICATE_ERROR;
+
+public final class Bonus {
+    private final Number value;
+
+    private Bonus(Number value) {
+        this.value = value;
+    }
+
+    public static Bonus of(Number bonus, Lotto winning) {
+        Validator.validateDuplicate(bonus, winning);
+        return new Bonus(bonus);
+    }
+
+    public Number getValue() {
+        return value;
+    }
+
+    private static class Validator {
+        private static void validateDuplicate(Number bonus, Lotto winning) {
+            if (isDuplicated(bonus, winning)) {
+                throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATE_ERROR.getMessage());
+            }
+        }
+
+        private static boolean isDuplicated(Number bonus, Lotto winning) {
+            return winning.getStream()
+                    .anyMatch(number ->
+                            number.equals(bonus));
+        }
+    }
+}
--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,7 +1,5 @@
 package lotto.domain;
 
-import static lotto.global.constans.ErrorMessage.BONUS_NUMBER_DUPLICATE_ERROR;
-
 public class WinningNumbers {
     private final Lotto winning;
     private final Number bonus;
@@ -12,21 +10,7 @@ public class WinningNumbers {
     }
 
     public static WinningNumbers of(Lotto winning, Number bonus) {
-        Validator.validateDuplicate(winning, bonus);
         return new WinningNumbers(winning, bonus);
     }
 
-    private static class Validator {
-        private static void validateDuplicate(Lotto winning, Number bonus) {
-            if (isDuplicated(winning, bonus)) {
-                throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATE_ERROR.getMessage());
-            }
-        }
-
-        private static boolean isDuplicated(Lotto winning, Number bonus) {
-            return winning.getStream()
-                    .anyMatch(number ->
-                            number.equals(bonus));
-        }
-    }
 }
