6c163232353b925204c018998692e51c331d3107
geniusjun <geniusjun4663@gmail.com>
2025-11-03 16:17:25 +0900

    Refactor(AppConfig): DI 컨테이너의 의미에 어울리는 로직으로 변경
    AppConfig 객체 생성시에 한번만 생성해놓고 공유하여 쓰는 것이 싱클톤 느낌도 나서 더 어울린다고 생각한다.

--- src/main/java/lotto/config/AppConfig.java
+++ src/main/java/lotto/config/AppConfig.java
@@ -12,12 +12,13 @@ import lotto.view.util.InputLoop;
  * 애플리케이션 객체의 의존 관계를 설정하고 조립하는 DI 컨테이너
  */
 public class AppConfig {
+    private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+    private final LottoFactory lottoFactory = new RandomLottoFactory();
+    private final LottoService lottoService = new LottoService(lottoFactory);
+    private final InputLoop inputLoop = new InputLoop(outputView);
+
     public LottoController lottoController() {
-        OutputView outputView = new OutputView();
-        InputView inputView = new InputView();
-        LottoFactory lottoFactory = new RandomLottoFactory();
-        LottoService lottoService = new LottoService(lottoFactory);
-        InputLoop inputLoop = new InputLoop(outputView);
         return new LottoController(outputView, inputView, lottoService, inputLoop);
     }
 }
