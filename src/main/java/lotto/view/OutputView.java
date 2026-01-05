a83d70af0eb620a3f2058170ccdf69f8d9dcf9e3
geniusjun <geniusjun4663@gmail.com>
2026-01-05 14:13:09 +0900

    Feat(LottoController): 기본적인 MVC 구조 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+public class OutputView {
+
+    public void printErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public void printlnMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printMessage(String message) {
+        System.out.print(message);
+    }
+}
